3908141f07e5a02cd768d741488ad8cc8c844a8c
the-javascript-jedi <nithin.thewebdev@gmail.com>
2021-03-16 23:57:33 +0530

    added Lottery exercise folder

--- dice-exercise/src/App.js
+++ dice-exercise/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import RollDice from './RollDice'
+// import RollDice from './RollDice'
 import './Die.css';
+import Lottery from './LotteryExercise/Lottery';
 function App() {
+
+
   return (
     <div className="App">
-      <RollDice/>
+      {/* dice example */}
+      {/* <RollDice/> */}
+      {/* Lottery Example */}
+      <Lottery/>     
+      <Lottery title="Mini Daily" maxNum={10} numBalls={4}/>     
     </div>
   );
 }
