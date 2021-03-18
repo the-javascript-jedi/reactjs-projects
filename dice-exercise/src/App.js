f1bedb73b528b058fc42b2b4fe1a1efb8463d6d4
the-javascript-jedi <nithin.thewebdev@gmail.com>
2021-03-18 22:28:32 +0530

    added css transition to coin flip game

--- dice-exercise/src/App.js
+++ dice-exercise/src/App.js
@@ -1,18 +1,17 @@
 import "./App.css";
-import RollDice from "./DiceExercise/RollDice";
-
+// import RollDice from "./DiceExercise/RollDice";
 // import Lottery from './LotteryExercise/Lottery';
-// import CoinContainer from "./CoinFlipperExercise/CoinContainer";
+import CoinContainer from "./CoinFlipperExercise/CoinContainer";
 function App() {
   return (
     <div className="App">
       {/* dice example */}
-      <RollDice />
+      {/* <RollDice /> */}
       {/* Lottery Example */}
       {/* <Lottery/>     
       <Lottery title="Mini Daily" maxNum={10} numBalls={4}/>      */}
       {/* Coin Flipper Exercise */}
-      {/* <CoinContainer /> */}
+      <CoinContainer />
     </div>
   );
 }
