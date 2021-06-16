cd1109c41c307b7a3bb9ef76d3efd7511070a73e
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-06-16 11:30:50 +0530

    added react router

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -2,17 +2,39 @@ import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 export class SongList extends Component {
+  // helper method to render songs
+  renderSongs() {
+    return this.props.data.songs.map((song) => {
+      return (
+        <li className="collection-item" key={song.id}>
+          {song.title}
+        </li>
+      );
+    });
+  }
   render() {
     console.log("this.props", this.props);
-    return <div>SongList</div>;
+    // return loading screen when graphql is loading the data
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <ul className="collection">
+        <h1>SongList</h1>
+        {this.renderSongs()}
+      </ul>
+    );
   }
 }
 // the helper from the gql library to form up the query itself.
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
 `;
+// this below code will send the query to graphql and return the data as props
 export default graphql(query)(SongList);
