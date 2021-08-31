bb7dc102dfd9632615e5d31df59b29e381aa0424
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-08-31 22:30:04 +0530

    added prod deployment files

--- Legacy React/WebpackProject-master/server.js
+++ Legacy React/WebpackProject-master/server.js
@@ -1,13 +1,33 @@
 const express = require("express");
-//the webpackMiddleware only serves to intercept incoming requests and hand it off to Webpack
-const webpackMiddleware = require("webpack-dev-middleware");
-//webpack exists to compile all of our application assets
-const webpack = require("webpack");
-//webpackConfig is what instructs webpack on how to run correctly.
-const webpackConfig = require("./webpack.config.js");
 
 const app = express();
-// apply the webpack middleware
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// routes example
+app.get("/hello", (req, res) => {
+  res.send({
+    hi: "there",
+  });
+});
 
-app.listen(3050, () => console.log("Listening"));
+// all routes must be specified above
+// development environment
+if (process.env.NODE_ENV !== "production") {
+  //the webpackMiddleware only serves to intercept incoming requests and hand it off to Webpack
+  const webpackMiddleware = require("webpack-dev-middleware");
+  //webpack exists to compile all of our application assets
+  const webpack = require("webpack");
+  //webpackConfig is what instructs webpack on how to run correctly.
+  const webpackConfig = require("./webpack.config.js");
+  // apply the webpack middleware only in development
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  // production environment
+  //that it should make everything inside of the dist directory freely available for use, to anyone who asks for it.
+  app.use(express.static("dist"));
+  //if anyone makes a get request to any route on our server,go ahead and send them back the index.html file.
+  //This statement right here is used specifically for compatibility with React Router specifically with the browser history module.
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist/index.html"));
+  });
+}
+// run a port dynamically or use fallback port
+app.listen(process.env.PORT || 3050, () => console.log("Listening"));
