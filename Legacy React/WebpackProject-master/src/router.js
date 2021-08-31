7e60f5de6c53b08b7440e32d0d29ea0758b8f860
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-08-31 16:48:06 +0530

    added code splitting for react

--- Legacy React/WebpackProject-master/src/router.js
+++ Legacy React/WebpackProject-master/src/router.js
@@ -3,10 +3,11 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import Home from "./components/Home";
 import ArtistMain from "./components/artists/ArtistMain";
-import ArtistDetail from "./components/artists/ArtistDetail";
-import ArtistCreate from "./components/artists/ArtistCreate";
-import ArtistEdit from "./components/artists/ArtistEdit";
-
+// Below modules are dynamically loaded
+// import ArtistDetail from "./components/artists/ArtistDetail";
+// import ArtistCreate from "./components/artists/ArtistCreate";
+// import ArtistEdit from "./components/artists/ArtistEdit";
+// React Route Codesplitting
 const componentRoutes = {
   component: Home,
   path: "/",
@@ -14,21 +15,47 @@ const componentRoutes = {
   childRoutes: [
     {
       path: "artists/new",
-      getComponent(location, cb) {},
+      getComponent(location, cb) {
+        // logic to fetch component
+        System.import("./components/artists/ArtistCreate").then((module) =>
+          cb(null, module.default)
+        );
+      },
+    },
+    {
+      path: "artists/:id",
+      getComponent(location, cb) {
+        // logic to fetch component
+        System.import("./components/artists/ArtistDetail").then((module) =>
+          cb(null, module.default)
+        );
+      },
+    },
+    {
+      path: "artists/:id/edit",
+      getComponent(location, cb) {
+        // logic to fetch component
+        System.import("./components/artists/ArtistEdit").then((module) =>
+          cb(null, module.default)
+        );
+      },
     },
   ],
 };
 
 const Routes = () => {
+  /* Existing Route before applying componentRoutes */
+  // <Router history={hashHistory}>
+  //   <Route path="/" component={Home}>
+  //     <IndexRoute component={ArtistMain} />
+  //     <Route path="artists/new" component={ArtistCreate} />
+  //     <Route path="artists/:id" component={ArtistDetail} />
+  //     <Route path="artists/:id/edit" component={ArtistEdit} />
+  //   </Route>
+  // </Router>
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    // Router using componentRoutes
+    <Router history={hashHistory} routes={componentRoutes} />
   );
 };
 
