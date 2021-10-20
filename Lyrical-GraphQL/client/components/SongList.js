a43bd5aab69ab70e6584ff2de9912cef0d195785
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-10-20 23:19:03 +0530

    added changes

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
+// graphql helper to connect react to graphql
 import { graphql } from "react-apollo";
 // router
 import { Link } from "react-router";
@@ -7,12 +8,25 @@ import { Link } from "react-router";
 import query from "../queries/fetchSongs";
 
 export class SongList extends Component {
+  // song delete function
+  onSongDelete(id) {
+    // call the mutation
+    this.props.mutate({
+      variables: { id: id },
+    });
+  }
   // helper method to render songs
   renderSongs() {
     return this.props.data.songs.map((song) => {
       return (
         <li className="collection-item" key={song.id}>
           {song.title}
+          <i
+            className="material-icons"
+            onClick={() => this.onSongDelete(song.id)}
+          >
+            delete
+          </i>
         </li>
       );
     });
@@ -37,6 +51,14 @@ export class SongList extends Component {
     );
   }
 }
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
 // we pass the imported fetchSongs graphql query
 // this below code will send the query to graphql and return the data as props
-export default graphql(query)(SongList);
+// this says make a helper using the graphql function and this mutation and then immediately invoke it with the result of this other helper and the song list
+export default graphql(mutation)(graphql(query)(SongList));
