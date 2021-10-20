f303623bf93059cc872413e8d3603f0d1ffe39a0
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-10-20 23:04:02 +0530

    added unchecked files

---
+++ lights-out/build/precache-manifest.00aac49d8196dd9cb1f7fe33575f93d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50ba2b5f98562af67a5d",
+    "url": "./static/css/main.8645897b.chunk.css"
+  },
+  {
+    "revision": "50ba2b5f98562af67a5d",
+    "url": "./static/js/main.50ba2b5f.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "./static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "69503449cd4e113864a8eb6163974dc3",
+    "url": "./index.html"
+  }
+];
