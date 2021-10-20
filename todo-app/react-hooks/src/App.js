25b8e996e4169d5bcb2820ecabfa5474dc6d95e3
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-10-20 23:20:38 +0530

    added hooks implementation

---
+++ todo-app/react-hooks/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import TodoList from "./TodoList/TodoList";
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+export default App;
