05c3848307844bd1aec9bab5f9b293d20af28fb7
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-10-27 09:28:32 +0530

    added drag and drop using vanilla js

--- react-drag-drop-js/src/App.js
+++ react-drag-drop-js/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Box from "./Box";
 
 const App = () => {
+  //dragId - keep track of which box it is we're currently dragging.(Box-1,Box-2,Box-3)
   const [dragId, setDragId] = useState();
   const [boxes, setBoxes] = useState([
     {
@@ -22,21 +23,44 @@ const App = () => {
   ]);
   useEffect(() => {
     console.log("boxes", boxes);
-  });
-  const handleDrag = (ev) => {
-    console.log("ev.currentTarget", ev.currentTarget);
-    console.log("ev.currentTarget.id", ev.currentTarget.id);
-    setDragId(ev.currentTarget.id);
+  }, [boxes]);
+  //update the order of boxes on textbox change
+  const updateOrder = (boxId) => {
+    let newBoxes = JSON.parse(JSON.stringify(boxes));
+    newBoxes.map(function (box, index) {
+      //initialize order of all boxes
+      box.order = index;
+      //update order of updated box
+      if (box.id === boxId) {
+        console.log("box.order updated", box.order);
+        box.order = newBoxes.length;
+      }
+      return null;
+    });
+    // newBoxes.find(function (x) {
+    //   if (x.id === boxId) {
+    //     console.log("x.order", x.order);
+    //     x.order = newBoxes.length + 1;
+    //   }
+    // });
+    console.log("newBoxes", newBoxes);
+    setBoxes(newBoxes);
   };
 
+  const handleDrag = (ev) => {
+    console.log("ev.currentTarget.id", ev.currentTarget.id); //Box-1||Box-2||Box-3
+    setDragId(ev.currentTarget.id);
+  };
   const handleDrop = (ev) => {
+    //identify which box is being dragged and which box it has been dropped on.
     const dragBox = boxes.find((box) => box.id === dragId);
     const dropBox = boxes.find((box) => box.id === ev.currentTarget.id);
-
+    //take note of original order of boxes
     const dragBoxOrder = dragBox.order;
     const dropBoxOrder = dropBox.order;
-
+    //rearrange the new boxes and return as a new array
     const newBoxState = boxes.map((box) => {
+      //check if the current box's id is equal to the dragId. If it is, set its order to be the dropBoxOrder. If not, check if it is equal to the id of the box being dropped on, and if that is true set its order to be the dragBoxOrder.
       if (box.id === dragId) {
         box.order = dropBoxOrder;
       }
@@ -45,10 +69,9 @@ const App = () => {
       }
       return box;
     });
-
+    //order variable for the two boxes involved has been swapped.
     setBoxes(newBoxState);
   };
-
   return (
     <div className="App">
       {boxes
@@ -60,10 +83,10 @@ const App = () => {
             boxNumber={box.id}
             handleDrag={handleDrag}
             handleDrop={handleDrop}
+            updateOrder={updateOrder}
           />
         ))}
     </div>
   );
 };
-
 export default App;
--- react-drag-drop-js/src/Box.js
+++ react-drag-drop-js/src/Box.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 
-const Box = ({ boxColor, boxNumber, handleDrag, handleDrop, order }) => {
+const Box = ({ boxColor, boxNumber, handleDrag, handleDrop, updateOrder }) => {
+  const [days, SetDays] = useState("Day 1");
+  //on update of box textbox send the box id to the parent component
+  const changeInputHandler = (event) => {
+    SetDays(event.target.value);
+    console.log("event.target.parentElement.id", event.target.parentElement.id);
+    //Box-1||Box-2||Box-3
+    updateOrder(event.target.parentElement.id);
+  };
   return (
     <div
+      // draggable sets whether the element can be dragged or not;
       draggable={true}
       id={boxNumber}
+      // onDragOver is an event listener triggered when you drag the element over something else;
       onDragOver={(ev) => ev.preventDefault()}
+      // onDragStart is an event listener triggered when you start dragging the element;
       onDragStart={handleDrag}
+      // onDrop is an event listener triggered when you drop the element;
       onDrop={handleDrop}
       style={{
         backgroundColor: boxColor,
@@ -19,7 +31,7 @@ const Box = ({ boxColor, boxNumber, handleDrag, handleDrop, order }) => {
       }}
     >
       {boxNumber}
-      <p>{order}</p>
+      <input type="text" value={days} onChange={changeInputHandler} />
     </div>
   );
 };
