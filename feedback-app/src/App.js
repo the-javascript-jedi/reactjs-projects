da812dc1ce3d244901e8986167bc0841ccf4ba36
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-01-31 09:49:10 +0530

    added routes

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
-import { v4 as uuidv4 } from "uuid";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+
 import { useState } from "react";
 
 const App = () => {
@@ -21,14 +26,30 @@ const App = () => {
     }
   };
   return (
-    <>
+    <BrowserRouter>
       <Header text="feedback ui"></Header>
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} deleteFeedback={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  deleteFeedback={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          {/* In react router 6 we need to pass jsx */}
+          <Route path="/about" element={<AboutPage />}></Route>
+        </Routes>
       </div>
-    </>
+      <AboutIconLink />
+    </BrowserRouter>
   );
 };
 
