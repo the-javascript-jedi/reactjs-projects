9282df79507dd17f32e08a97226702109ceb16ec
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-02-23 17:09:35 +0530

    setup redux file structure

---
+++ pokeapi-react-redux/src/reducer/RootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+const RootReducer = combineReducers({
+  reducers: {},
+});
+export default RootReducer;
