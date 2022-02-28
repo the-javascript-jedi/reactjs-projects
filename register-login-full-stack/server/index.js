7c9c71c1d85e88faa671e848668f5e33335af7cf
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-02-28 16:22:10 +0530

    added password hashing

--- register-login-full-stack/server/index.js
+++ register-login-full-stack/server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 // const mysql = require("mysql");
 //due to security issue using mysql2
 const mysql = require("mysql2");
@@ -21,27 +23,50 @@ const db = mysql.createPool({
 app.post("/register", (req, res) => {
   const userName = req.body.username;
   const passWord = req.body.password;
-  const sqlInsert = "INSERT INTO users (username,password) VALUES  (?,?)";
-  db.query(sqlInsert, [userName, passWord], (err, result) => {
-    console.log("err", err);
-    console.log("result", result);
-    res.send("Inserted");
+  //hash the password with saltRounds
+  bcrypt.hash(passWord, saltRounds, (err, hash) => {
+    // Store hash in your password DB.
+    if (err) {
+      console.log("err-/register", err);
+    } else {
+      const sqlInsert = "INSERT INTO users (username,password) VALUES  (?,?)";
+      // pass the hashed variable
+      db.query(sqlInsert, [userName, hash], (err, result) => {
+        console.log("err", err);
+        console.log("result", result);
+        res.send("Inserted");
+      });
+    }
   });
 });
 // Login user
 app.post("/login", (req, res) => {
   const userName = req.body.username;
   const passWord = req.body.password;
-  const sqlSelect = "SELECT * FROM users where username=? AND password=?";
-  db.query(sqlSelect, [userName, passWord], (err, result) => {
+  //get the username
+  const sqlSelect = "SELECT * FROM users where username=?";
+  //   hash password using bcrypt
+  db.query(sqlSelect, [userName], (err, result) => {
     if (err) {
       res.send({ err: err });
     }
     // check the sql query if it returns an empty array
     if (result.length > 0) {
-      res.send(result);
+      // check if combination is correct for password by comparing typed password with db hashed password
+      bcrypt.compare(
+        passWord,
+        result[0].password,
+        (error, passwordResponse) => {
+          if (passwordResponse) {
+            res.send(result);
+          } else {
+            res.send({ message: "Wrong username/password combination" });
+          }
+        }
+      );
     } else {
-      res.send({ message: "Wrong username/password combination" });
+      //username is not present in db
+      res.send({ message: "User does not exist" });
     }
   });
 });
