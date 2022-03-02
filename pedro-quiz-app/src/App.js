6ab8b82795f4f31a8eb541f95c6914e9a11312d1
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-03-02 16:32:34 +0530

    Initial Commit

---
+++ pedro-quiz-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { useState } from "react";
+import MainMenu from "./Components/MainMenu";
+import Quiz from "./Components/Quiz";
+import EndScreen from "./Components/EndScreen";
+function App() {
+  const [gameState, setGameState] = useState("menu");
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+      {gameState === "menu" && <MainMenu />}
+      {gameState === "quiz" && <Quiz />}
+      {gameState === "endScreen" && <EndScreen />}
+    </div>
+  );
+}
+
+export default App;
