3b0da8d626a79b4a88e830c5dfcce1905424a1e6
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-10-23 16:43:57 +0530

    added basics

--- brad_traversy/youtube/react-crash-course/src/App.jsx
+++ brad_traversy/youtube/react-crash-course/src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar';
 
 const App = () => {
+  const names=["nithin","winpray","rian"];
+  const loggedIn=true;
+
   return (
     <div className='text-5xl'>
-      Hello
+      <p style={{color:'red',fontSize:'24px'}}>
+        Hello
+      </p>
+      {/* for loop */}
+      <ul>
+        {
+          names.map((name,index)=>(
+            <li key={index}>
+              {name}
+            </li>
+          ))
+        }
+      </ul>
+      {/* && check */}
+      {
+        loggedIn&&<h1>Hello Member</h1>
+      }
+      {/* ternary operator check */}
+      {
+        loggedIn?<h1>Hello ternary</h1>:""
+      }
     </div>
   )
 }
