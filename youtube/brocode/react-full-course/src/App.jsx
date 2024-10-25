db3f61ac72b2622573b88b6dcb61c73e7f6597cb
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-10-25 20:23:54 +0530

    added onchange

--- youtube/brocode/react-full-course/src/App.jsx
+++ youtube/brocode/react-full-course/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Button from "./Button";
 import MyComponent from "./MyComponent";
-import Counter from "./Counter";
 const App = () => {
   return (
     <>
-      <Counter />
+      <MyComponent />
     </>
   );
 };
--- youtube/brocode/react-full-course/src/MyComponent.jsx
+++ youtube/brocode/react-full-course/src/MyComponent.jsx
@@ -1,38 +1,76 @@
 import React, { useState } from "react";
+
 const MyComponent = () => {
-  const [name, setName] = useState("Guest");
-  const [age, setAge] = useState(0);
-  const [isEmployed, setIsEmployed] = useState(true);
+  // input state
+  const [name, setName] = useState("");
+  // select state
+  const [payment, setPayment] = useState("Mastercard");
+  // radio button state
+  const [shipping, setShipping] = useState("PickUp");
 
-  const updateName = () => {
-    setName("Spongebob");
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
 
-  const incrementAge = () => {
-    setAge(age + 1);
-  };
-  const toggleStatus = () => {
-    setIsEmployed(!isEmployed);
+  const handlePaymentChange = (e) => {
+    setPayment(e.target.value);
   };
 
+  const handleShippingChange = (e) => {
+    setShipping(e.target.value);
+  };
   return (
     <>
+      {/* input */}
       <div>
-        <div>Name: {name}</div>
-        <button onClick={() => updateName()}>Set Name</button>
-      </div>
-      <div>
-        {" "}
+        <input type="text" onChange={(e) => handleNameChange(e)} />
         <br />
-        Age : {age}
-        <button onClick={() => incrementAge()}>Increment Age</button>
+        Name: {name}
+      </div>
+      {/* select */}
+      <div>
+        <select
+          value={payment}
+          onChange={(e) => {
+            handlePaymentChange(e);
+          }}
+        >
+          <option value="">Select a Value</option>
+          <option value="Visa">Visa</option>
+          <option value="Mastercard">Mastercard</option>
+          <option value="Giftcard">Giftcard</option>
+        </select>
+        <br />
+        <div>{payment}</div>
+      </div>
+      {/* radio button */}
+      <div>
+        <label>
+          <input
+            type="radio"
+            value="Delivery"
+            onChange={(e) => {
+              handleShippingChange(e);
+            }}
+            checked={shipping == "Delivery"}
+          />{" "}
+          Delivery
+        </label>
       </div>
       <div>
-        {" "}
-        <br />
-        isEmployed : {isEmployed ? "Yes" : "No"}
-        <button onClick={() => toggleStatus()}>Toggle Status</button>
+        <label>
+          <input
+            type="radio"
+            value="PickUp"
+            onChange={(e) => {
+              handleShippingChange(e);
+            }}
+            checked={shipping == "PickUp"}
+          />{" "}
+          Pick Up
+        </label>
       </div>
+      <div>Shipping: {shipping}</div>
     </>
   );
 };
