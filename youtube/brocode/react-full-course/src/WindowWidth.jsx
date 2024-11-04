14a7a259761d0709a789943852ce3a228f432fef
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-11-04 14:25:06 +0530

    added useEffect height

--- youtube/brocode/react-full-course/src/WindowWidth.jsx
+++ youtube/brocode/react-full-course/src/WindowWidth.jsx
@@ -2,6 +2,27 @@ import React, { useState, useEffect } from "react";
 const WindowWidth = () => {
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
+  // Add event listener to update width and height
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    console.log("EVENT LISTENER ADDED");
+    return () => {
+      console.log("EVENT LISTENER REMOVED");
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  //   update the title with height and width
+  useEffect(() => {
+    document.title = `${width} X ${height}`;
+  }, [width, height]);
+
   return (
     <div>
       WindowWidth
