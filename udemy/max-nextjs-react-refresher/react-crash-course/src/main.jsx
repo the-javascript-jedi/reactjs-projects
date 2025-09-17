0d0ec25fcacd288e1185812bfba56619a77d57f3
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-09-17 10:00:00 +0530

    moved mock db

--- udemy/max-nextjs-react-refresher/react-crash-course/src/main.jsx
+++ udemy/max-nextjs-react-refresher/react-crash-course/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import Post from "./routes/Posts.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./routes/RootLayout.jsx";
+import NewPost from "./routes/NewPost/NewPost.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Post /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
