5f3f03ea9edc5bdbcbc7edefa38f2c76b367a2bf
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-10-28 19:51:46 +0530

    added classes for styles

--- udemy/max-nextjs-react-refresher/react-crash-course/src/App.jsx
+++ udemy/max-nextjs-react-refresher/react-crash-course/src/App.jsx
@@ -1,12 +1,14 @@
-import Post from "./components/Post";
+import Post from "./components/Post/Post";
+import PostList from "./components/PostList/PostList";
 
 function App() {
   return (
     <>
       <h1>Hello world</h1>
-      <Post author={"nithin"} body="reactjs" />
+      {/* <Post author={"nithin"} body="reactjs" />
       <Post author={"batman"} body="python" />
-      <Post author={"superman"} body="angular" />
+      <Post author={"superman"} body="angular" /> */}
+      <PostList />
     </>
   );
 }
---
+++ udemy/max-nextjs-react-refresher/react-crash-course/src/components/PostList/PostList.jsx
@@ -0,0 +1,22 @@
+import Post from "../Post/Post";
+import classes from "./Postlist.module.css";
+
+const PostList = () => {
+  return (
+    <ol className={classes.posts}>
+      <li>
+        <Post author={"nithin"} body="reactjs" />
+      </li>
+      <li>
+        {" "}
+        <Post author={"batman"} body="python" />
+      </li>
+      <li>
+        {" "}
+        <Post author={"superman"} body="angular" />{" "}
+      </li>
+    </ol>
+  );
+};
+
+export default PostList;
