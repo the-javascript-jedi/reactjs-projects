f05d2f9f57d4d702406b27a97c8182d4a8200c78
nithin.thewebdev <nithin.thewebdev@gmail.com>
2026-04-12 11:15:55 +0530

    added validation

--- ember-forge/udemy-courses/brad-traversy/github-finder/src/App.jsx
+++ ember-forge/udemy-courses/brad-traversy/github-finder/src/App.jsx
@@ -1,15 +1,26 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
-import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <>
       <Router>
         <div className="flex flex-col justify-between h-screen">
           <Navbar title={"github finder"} />
-          <main className="container mx-auto px-3 pb-12">Content</main>
+          <main className="container mx-auto px-3 pb-12">
+            Content
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </div>
       </Router>
